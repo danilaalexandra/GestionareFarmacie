9059a5ad321f58d56d29c53471479fb131de4812
danilaalexandra <viorica.danila@student.usv.ro>
2025-04-09 10:33:44 +0300

    Add files via upload

---
+++ FarmacieLibrarie/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace FarmacieLibrarie
+{
+    public class Client
+    {
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+
+
+        private const int NUME = 0;
+        private const int PRENUME = 1;
+        private const int DATA_NASTERII = 2;
+
+        public string nume { get; set; }
+        public string prenume { get; set; }
+        public DateTime data_nasterii { get; set; }
+
+        public Client()
+        {
+            nume = string.Empty;
+            prenume = string.Empty;
+            data_nasterii = DateTime.MinValue;
+        }
+        public Client(string _nume, string _prenume, DateTime _data_nasterii)
+        {
+            nume = _nume;
+            prenume = _prenume;
+            data_nasterii = _data_nasterii;
+        }
+        public string Info()
+        {
+            return $"Numele Clientului : {nume} {prenume}, data nasterii a clientului : {data_nasterii}";
+        }
+        public Client(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+
+            this.nume = dateFisier[NUME];
+            this.prenume = dateFisier[PRENUME];
+            this.data_nasterii = DateTime.ParseExact(dateFisier[DATA_NASTERII],"dd/MM/yyyy",System.Globalization.CultureInfo.InvariantCulture);
+        }
+        public string ConversieLaSir_PentruFisier()
+        {
+            string obiectClientPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}",
+                SEPARATOR_PRINCIPAL_FISIER,
+                (nume ?? " NECUNOSCUT "),
+                (prenume ?? " NECUNOSCUT "),
+                data_nasterii.ToString("dd/MM/yyyy"));
+
+            return obiectClientPentruFisier;
+        }
+        public static bool ValidareNume(string nume)
+        {
+            if (string.IsNullOrWhiteSpace(nume))
+                return false;
+            else return true;
+        }
+        public static bool ValidarePrenume(string prenume)
+        {
+            if (string.IsNullOrWhiteSpace(prenume))
+                return false;
+            else return true;
+        }
+
+
+    }
+}
---
+++ FarmacieLibrarie/Medicament.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace FarmacieLibrarie
+
+{
+    public class Medicament
+    {
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+        private const int DENUMIRE = 0;
+        private const int PRET = 1;
+        private const int NECESITARETETA = 2;
+        private const int CATEGORIE = 3;
+        public string denumire { get; set; }
+        public float pret { get; set; }
+        public bool necesitaReteta { get; set; }
+        public Categorie categorie { set; get; }
+
+
+
+
+        public Medicament()
+        {
+            denumire = string.Empty;
+            pret = 0;
+            necesitaReteta = false;
+
+        }
+
+        public Medicament(string _denumire, float _pret, bool _necesitaReteta)
+        {
+            denumire = _denumire;
+            pret = _pret;
+            necesitaReteta = _necesitaReteta;
+
+        }
+
+        public string InfoMedicament()
+        {
+            return $"Denumirea medicamentului: {denumire}, Pretul medicamentului: {pret} , Reteta: {necesitaReteta} , Categorie : {categorie}";
+        }
+
+        public Medicament(string linieFisier)
+        {
+            var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+
+            this.denumire = dateFisier[DENUMIRE];
+            this.pret = float.Parse(dateFisier[PRET].Replace(',', '.'), CultureInfo.InvariantCulture);
+            string retetaStr = dateFisier[NECESITARETETA].Trim().ToLower();
+            this.necesitaReteta = (retetaStr == "da" || retetaStr == "true" || retetaStr == "1");
+            this.categorie = (Categorie)Enum.Parse(typeof(Categorie), dateFisier[CATEGORIE]);
+        }
+
+        public string ConversieLaSir_PentruFisier()
+        {
+            string obiectMedicamentPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}",
+                SEPARATOR_PRINCIPAL_FISIER,
+                (denumire ?? " NECUNOSCUT "),
+                pret.ToString(CultureInfo.InvariantCulture),
+                (necesitaReteta ? "da" : "nu"),
+                categorie.ToString());
+
+            return obiectMedicamentPentruFisier;
+        }
+
+        public static bool ValidareDenumire(string denumire)
+        {
+            if (string.IsNullOrWhiteSpace(denumire))
+                return false;
+            else return true;
+
+        }
+    }
+}
