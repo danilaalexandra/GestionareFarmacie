19cc02b90659d36bcb1aafaf528a8d471c605cde
danilaalexandra <viorica.danila@student.usv.ro>
2025-04-09 11:09:35 +0300

    Add files via upload

---
+++ FormClient.cs
@@ -0,0 +1,100 @@
+﻿using FarmacieLibrarie;
+using NivelStocareDate;
+using System;
+using System.Windows.Forms;
+
+namespace FarmacieInterfata
+{
+    public partial class FormClient : Form
+    {
+        private AdministrareClientFisier adminClienti;
+
+        public FormClient()
+        {
+            InitializeComponent();
+            adminClienti = new AdministrareClientFisier("clienti.txt");
+
+           
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+        }
+
+        private bool ValidareInput()
+        {
+            bool valid = true;
+            errorProvider1.Clear();
+
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                errorProvider1.SetError(textBox1, "Prenumele este obligatoriu!");
+                label4.Text = "Prenumele este obligatoriu!";
+                label4.Visible = true;
+                valid = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                errorProvider1.SetError(textBox2, "Numele este obligatoriu!");
+                label5.Text = "Numele este obligatoriu!";
+                label5.Visible = true;
+                valid = false;
+            }
+
+            if (dateTimePicker1.Value > DateTime.Today)
+            {
+                errorProvider1.SetError(dateTimePicker1, "Data nașterii nu a fost adăugată!");
+                label6.Text = "Data nașterii nu a fost adăugată!";
+                label6.Visible = true;
+                valid = false;
+            }
+
+            return valid;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!ValidareInput())
+            {
+                return;
+            }
+
+            try
+            {
+                Client client = new Client(textBox1.Text.Trim(), textBox2.Text.Trim(), dateTimePicker1.Value);
+                adminClienti.AddClient(client);
+                this.Hide();
+                FormAfisareClienti formAfisare = new FormAfisareClienti();
+                formAfisare.Show();
+
+                MessageBox.Show("Client adăugat cu succes!", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                
+                textBox1.Clear();
+                textBox2.Clear();
+                dateTimePicker1.Value = DateTime.Today;
+                textBox1.Focus();
+
+                errorProvider1.Clear();
+                label4.Visible = false;
+                label5.Visible = false;
+                label6.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la salvare: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_click(object sender, EventArgs e)
+        {
+            FormAfisareClienti form = new FormAfisareClienti();
+            form.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ FormMedicament.cs
@@ -0,0 +1,131 @@
+﻿using FarmacieLibrarie;
+using NivelStocareDate;
+using System;
+using System.Windows.Forms;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FarmacieInterfata
+{
+    public partial class FormMedicament : Form
+    {
+        private AdministrareMedicamentFisier adminMedicamente;
+
+        public FormMedicament()
+        {
+            InitializeComponent();
+            adminMedicamente = new AdministrareMedicamentFisier("medicamente.txt");
+
+            
+            InitializareComboBoxCategorie();
+        }
+        private void InitializareComboBoxCategorie()
+        {
+            List<object> categorii = new List<object>();
+            categorii.Add("-- Selectează categoria --");
+            categorii.AddRange(Enum.GetValues(typeof(Categorie)).Cast<object>());
+            comboBox1.DataSource = categorii;
+            comboBox1.SelectedIndex = 0;
+        }
+
+
+        private bool ValidareInput()
+        {
+            bool valid = true;
+            errorProvider1.Clear();
+
+            
+            label5.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label8.Visible = false;
+
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                errorProvider1.SetError(textBox1, "Denumirea este obligatorie!");
+                label5.Text = "Denumirea este obligatorie!";
+                label5.Visible = true;
+                valid = false;
+            }
+
+            if (!radioButton1.Checked && !radioButton2.Checked)
+            {
+                errorProvider1.SetError(radioButton2, "Selectați dacă necesită rețetă!");
+                label7.Text = "Selectați dacă necesită rețetă!";
+                label7.Visible = true;
+                valid = false;
+            }
+            if (numericUpDown1.Value <= 0)
+            {
+                errorProvider1.SetError(numericUpDown1, "Introduceți un preț mai mare decât 0!");
+                label6.Text = "Introduceți un preț mai mare decât 0!";
+                label6.Visible = true;
+                valid = false;
+            }
+
+            if (!(comboBox1.SelectedItem is Categorie))
+            {
+                errorProvider1.SetError(comboBox1, "Selectați o categorie!");
+                label8.Text = "Selectați o categorie!";
+                label8.Visible = true;
+                valid = false;
+            }
+
+            return valid;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!ValidareInput())
+            {
+                return;
+            }
+
+            try
+            {
+                string denumire = textBox1.Text.Trim();
+                float pret = (float)numericUpDown1.Value;
+                bool necesitaReteta = radioButton1.Checked;
+                Categorie categorieAleasa = (Categorie)comboBox1.SelectedItem;
+
+                Medicament medicament = new Medicament(denumire, pret, necesitaReteta)
+                {
+                    categorie = categorieAleasa
+                };
+
+                adminMedicamente.AddMedicament(medicament);
+                this.Hide();
+                FormAfisareMedicamente formAfisare = new FormAfisareMedicamente();
+                formAfisare.Show();
+
+                MessageBox.Show("Medicament adăugat cu succes!", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                
+                textBox1.Clear();
+                numericUpDown1.Value = 0;
+                radioButton1.Checked = false;
+                radioButton2.Checked = false;
+                comboBox1.SelectedIndex = -1;
+                textBox1.Focus();
+
+                errorProvider1.Clear();
+                label4.Visible = false;
+                label6.Visible = false;
+                label8.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eroare la salvare: " + ex.Message, "Eroare", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_click(object sender, EventArgs e)
+        {
+            FormAfisareMedicamente form = new FormAfisareMedicamente();
+            
+            form.Show();
+            this.Hide();
+        }
+    }
+}
